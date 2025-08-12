8c40932dd62348c69e4bfa193f1042f5fd7bb48a
Marcos Nunes <marcosasn3991@gmail.com>
2025-08-12 17:20:41 +0000

    e remover o icone atual, é possível?

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,7 +16,6 @@ export default function RootLayout({
   return (
     <html lang="pt-BR" className="scroll-smooth">
       <head>
-        <link rel="icon" href="/favicon.ico" sizes="any" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
         <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;500;600;700&display=swap" rel="stylesheet" />
